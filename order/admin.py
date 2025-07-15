f4b640c2740a2ff32d63a15abb4affc771201f2b
elmira <elmira.nikroodi96@gmial.com>
2025-07-15 14:20:20 +0330

    order models

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Order , OrderItem
 # Register your models here.
+
+class OrderItemInLine(admin.TabularInline):
+    model = OrderItem
+    row_id_field = ("product",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id","user","updated","paid",)
+    list_filter= ("paid",)
+    inlines = (OrderItemInLine,)
