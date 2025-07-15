a8abecfbb9d56b1db6f8f6b21b7ff875ebe0a97f
elmira <elmira.nikroodi96@gmial.com>
2025-07-15 15:43:50 +0330

    order checkout

--- order/views.py
+++ order/views.py
@@ -4,6 +4,8 @@ from .cart import Cart
 from django.shortcuts import get_object_or_404
 from products.models import Product
 from .forms import CartAddForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Order , OrderItem
 # Create your views here.
 
 class CartView(View):
@@ -30,3 +32,18 @@ class CartRemoveView(View):
         product = get_object_or_404(Product , id=product_id)
         cart.remove(product)
         return redirect("order:cart")
+
+class OrderCreateView(LoginRequiredMixin ,View):
+    def get(self , request):
+        cart = Cart(request)
+        order = Order.objects.create(user=request.user)
+        for item in cart :
+            OrderItem.objects.create(order = order , product=item['product'],price=item['price'],quantity=item['quantity'])
+        cart.clear()
+        return redirect("order:order_detail",order.id)
+
+
+class OrderDetailView(LoginRequiredMixin,View):
+    def get(self , request , order_id ):
+        order = get_object_or_404(Order , id = order_id)
+        return render(request , "order/order.html",{"order":order})
